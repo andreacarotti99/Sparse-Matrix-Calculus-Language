36bb0b7f4efa945cf828a016f960d6a98707f1a0
Sofia Lucca <79834548+sofialucca@users.noreply.github.com>
2022-11-11 16:44:01 -0600

    added parser function

--- base.ml
+++ base.ml
@@ -14,18 +14,20 @@ type cmd = Assign of ident * exp | Seq of cmd * cmd | Skip
            (*| Switch of exp * (int * cmd) list * cmd *)
            | CreateCOO of ident * exp * exp * exp
 
-type coodecl = {varname : ident; row : exp; cols : exp; data : exp}
+type coodecl = {row : exp; cols : exp; data : exp}
 
 (* type checker *)
 type typ = IntTy | BoolTy | VectorTy | COO of coodecl
 
 type context = ident -> typ option
 let empty_context = fun x -> None
-let lookup (gamma : context) (x : ident) : typ option = gamma x
+let lookup_context (gamma : context) (x : ident) : typ option = gamma x
+let update_context (gamma : context) (x : ident) (e : typ) = fun y -> if y = x then Some e else gamma y
 
 let lookup_coo (gamma : context) (x : ident) : coodecl option =
-  match lookup gamma x with 
-    Some (COO cd) -> Some cd 
+  match lookup_context gamma x with 
+    | Some (COO cd) -> Some cd 
+    | _ -> None
 
 
 
@@ -62,7 +64,7 @@ l1 = [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4]
 returns the array without zeros:
 l1 = [1; 7; 2; 8; 5; 3; 9; 6; 4]
 *)
-let rec get_data_array content =
+let rec get_data_array (content:int list): int list =
   match content with
   | [] -> []
   | head :: tail -> (match head with
@@ -76,7 +78,7 @@ l1 = [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4]
 returns the columns of the non-zero elements in an array
 l1 = [0; 1; 1; 2; 0; 2; 3; 1; 3]
 *)
-let rec get_cols_array content num_cols = 
+let rec get_cols_array (content:int list) (num_cols:int): int list = 
   match content with
     | [] -> []
     | head :: tail -> (match head with
@@ -89,7 +91,7 @@ l1 = [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4]
 returns the rows of the non-zero elements in an array
 l1 = [0; 0; 1; 1; 2; 2; 2; 3; 3]
 *)
-let rec get_rows_array content num_rows num_cols = 
+let rec get_rows_array (content:int list) (num_rows: int) (num_cols:int): int list = 
   match content with
     | [] -> []
     | head :: tail -> (match head with
@@ -97,7 +99,10 @@ let rec get_rows_array content num_rows num_cols =
       | _ -> (get_row (get_size content) num_rows num_cols) :: (get_rows_array tail num_rows num_cols))
       
 
-(* ------------------------------------------------------------------------------------------------------------------------------------------------ *)
+let parse_coo (num_rows:int) (num_cols:int) (v:int list): coodecl =
+  {row = Vector(get_rows_array v num_rows num_cols); cols = Vector(get_cols_array v num_cols); data = Vector(get_data_array v)}
+
+  (* ------------------------------------------------------------------------------------------------------------------------------------------------ *)
 
 let rec type_of (gamma : context) (e : exp) : typ option =
   match e with
@@ -106,7 +111,7 @@ let rec type_of (gamma : context) (e : exp) : typ option =
       (match type_of gamma e1, type_of gamma e2 with
        | Some IntTy, Some IntTy -> Some IntTy
        | _, _ -> None)
-  | Var x -> lookup gamma x
+  | Var x -> lookup_context gamma x
   | Bool _ -> Some BoolTy
   | And (e1, e2) | Or (e1, e2) ->
       (match type_of gamma e1, type_of gamma e2 with
@@ -116,11 +121,12 @@ let rec type_of (gamma : context) (e : exp) : typ option =
       (match type_of gamma e1, type_of gamma e2 with
        | Some t1, Some t2 -> if t1 = t2 then Some BoolTy else None
        | _, _ -> None)
+  | Vector v -> Some VectorTy
   
 let rec typecheck_cmd (gamma : context) (c : cmd) : bool =
   match c with
   | Assign (i, e) ->
-      (match lookup gamma i, type_of gamma e with
+      (match lookup_context gamma i, type_of gamma e with
        | Some t1, Some t2 -> t1 = t2
        | _, _ -> false)
   | Seq (c1, c2) -> typecheck_cmd gamma c1 && typecheck_cmd gamma c2
@@ -134,18 +140,19 @@ let rec typecheck_cmd (gamma : context) (c : cmd) : bool =
 type value = IntVal of int | BoolVal of bool | VectorVal of int list 
 
 type entry = Val of value | Fun of ident list * cmd | COO of coodecl
-
+(* state *)
 type state = ident -> entry option
 let empty_state = fun x -> None
-let lookup (s : state) (x : ident) : entry option = s x
-let update (s : state) (x : ident) (e : entry) : state = fun y -> if y = x then Some e else s y
+let lookup_state (s : state) (x : ident) : entry option = s x
+let update_state (s : state) (x : ident) (e : entry) : state = fun y -> if y = x then Some e else s y
 
-let update_coo (gamma : context) (x : ident) (c : coodecl) : context = update gamma x (COO coo)
+
+let update_coo (gamma : context) (x : ident) (coo : coodecl) : context = update_context gamma x (COO coo)
 
 
 let rec eval_exp (e : exp) (s : state) : value option =
   match e with
-  | Var x -> (match lookup s x with Some (Val v) -> Some v | _ -> None)
+  | Var x -> (match lookup_state s x with Some (Val v) -> Some v | _ -> None)
   | Num i -> Some (IntVal i)
   | Add (e1, e2) -> (match eval_exp e1 s, eval_exp e2 s with
                      | Some (IntVal i1), Some (IntVal i2) -> Some (IntVal (i1 + i2))
@@ -163,6 +170,7 @@ let rec eval_exp (e : exp) (s : state) : value option =
   | Eq (e1, e2) -> (match eval_exp e1 s, eval_exp e2 s with
                      | Some v1, Some v2 -> Some (BoolVal (v1 = v2))
                      | _, _ -> None)
+  | Vector v -> Some(VectorVal v)
 
 let rec eval_exps (es : exp list) (s : state) : value list option =
   match es with
@@ -173,7 +181,7 @@ let rec eval_exps (es : exp list) (s : state) : value list option =
 
 let rec add_args (s : state) (li : ident list) (lv : value list) : state =
   match li, lv with
-  | i :: irest, v :: vrest -> add_args (update s i (Val v)) irest vrest
+  | i :: irest, v :: vrest -> add_args (update_state s i (Val v)) irest vrest
   | _, _ -> s
 
 type stack = (state * ident) list
@@ -183,7 +191,7 @@ type config = cmd * stack * state
 let rec step_cmd (c : cmd) (k : stack) (s : state) : config option =
   match c with
   | Assign (x, e) -> (match eval_exp e s with
-                      | Some v -> Some (Skip, k, update s x (Val v))
+                      | Some v -> Some (Skip, k, update_state s x (Val v))
                       | None -> None)
   | Seq (Skip, c2) -> Some (c2, k, s)
   | Seq (c1, c2) -> (match step_cmd c1 k s with
@@ -196,17 +204,17 @@ let rec step_cmd (c : cmd) (k : stack) (s : state) : config option =
                         | _ -> None)
   | While (e, c) -> Some (IfC (e, Seq (c, While (e, c)), Skip), k, s)
   | Call (x, f, es) -> (match eval_exps es s with
-                        | Some vs -> (match lookup s f with
+                        | Some vs -> (match lookup_state s f with
                                       | Some (Fun (params, c)) -> Some (c, (s, x) :: k, add_args s params vs)
                                       | _ -> None)
                         | None -> None)
   | Return e -> (match eval_exp e s, k with
-                 | Some v, (s', x) :: k' -> Some (Skip, k', update s' x (Val v))
+                 | Some v, (s', x) :: k' -> Some (Skip, k', update_state s' x (Val v))
                  | _, _ -> None)
   | CreateCOO (x, num_row, num_col, content) -> 
-      (match eval_exp content s with
-          | Some v -> Some (Skip, k, update s x (COO (parse_coo num_row num_col content)) )
-          | None -> None)
+      (match eval_exp content s,eval_exp num_row s,eval_exp num_col s with
+          | Some( VectorVal v ), Some(IntVal r),Some(IntVal c)-> Some (Skip, k, update_state s x (COO (parse_coo r c v)) )
+          | _ -> None)
 
 
 
@@ -219,19 +227,21 @@ let rec run_config (con : config) : config =
 let run_prog (c : cmd) s =
   run_config (c, [], s)
 
-let state0 = update empty_state "f" (Fun (["x"; "y"], Return (Add (Var "x", Var "y"))))
+let state0 = update_state empty_state "f" (Fun (["x"; "y"], Return (Add (Var "x", Var "y"))))
 
-let state1 = update (update state0 "x" (Val (IntVal 1)))
+let state1 = update_state (update_state state0 "x" (Val (IntVal 1)))
   "y" (Val (IntVal 2))
   
-let config1 = (Return (Add (Var "x", Var "y")), [(state0, "x")], state1)
-
+let config1 =( CreateCOO("z",Num 4, Num 4, Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4]), [(state0, "x")], state1)
+let config2 = (Assign("s", Add(Num 4, Num 4)), [(state0, "x")], state1)
 let prog1 = Call ("x", "f", [Num 1; Num 2])
 
 let (res_c, res_k, res_s) = run_config config1;;
-lookup res_s "x";; (* should return Some (Val (IntVal 3)) *)
-lookup res_s "y";; (* should return None *)
+lookup_state res_s "z";; (* should return Some (Val (IntVal 3)) *)
 
+let (res_c, res_k, res_s) = run_config config2;;
+lookup_state res_s "s";; (* should return Some (Val (IntVal 3)) *)
+(*
 let (res_c, res_k, res_s) = run_prog prog1 state0;;
-lookup res_s "x";; (* should return Some (Val (IntVal 3)) *)
-lookup res_s "y";; (* should return None *)
+lookup_state res_s "x";;  should return Some (Val (IntVal 3))
+lookup_state res_s "y";; should return None *)
