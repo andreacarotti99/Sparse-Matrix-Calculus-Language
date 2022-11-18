d251eeb6e2effe3691a077c2b120e84569588634
Sofia Lucca <79834548+sofialucca@users.noreply.github.com>
2022-11-18 14:22:26 -0600

    finished typecheck

--- typecheck_cmd_test.ml
+++ typecheck_cmd_test.ml
@@ -47,3 +47,40 @@ let den_0 = type_of ctx (DensityCOO (Var "d1")) (* Shoul return Some FloatTy *)
 let den_1 = type_of ctx (DensityCOO (Var "x")) (* Shoul return None *)
 let den_2 = type_of ctx (DensityCOO (Var "v")) (* Shoul return None *)
 
+(* Test for Assign *)
+let ctx = update_context ctx "x" IntTy
+let asgn_0 = typecheck_cmd ctx (Assign("v", Vector ([1;2;3]))) (* Should return true*)
+let asgn_1 = typecheck_cmd ctx (Assign("d1", Var "d2")) (* Should return true*)
+let asgn_2 = typecheck_cmd ctx (Assign("d1", Num 7)) (* Should return false*)
+
+(*Test for CreateCOO*)
+let ctx = update_context ctx "d3" COOTy
+let crcoo_0 = typecheck_cmd ctx (CreateCOO("d1",Num 4, Num 4, Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return true*)
+let crcoo_1 = typecheck_cmd ctx (CreateCOO("d1",Var "x", Num 4, Var "v")) (* Should return true*)
+let crcoo_2 = typecheck_cmd ctx (CreateCOO("x",Num 4, Num 4, Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return false*)
+let crcoo_3 = typecheck_cmd ctx (CreateCOO("d1",Num 4,Bool true, Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return false*)
+let crcoo_3 = typecheck_cmd ctx (CreateCOO("d1",Num 4,Num 2, Num 3 )) (* Should return false*)
+
+(*Test MatSumCOO*)
+
+let sumcoo_0 = typecheck_cmd ctx (MatSumCOO("d1",Var "d1", Var "d2")) (* Should return true*)
+let sumcoo_1 = typecheck_cmd ctx (MatSumCOO("d3",Var "d1", Var "d2")) (* Should return true*)
+let sumcoo_2 = typecheck_cmd ctx (MatSumCOO("v",Var "d1", Var "d2")) (* Should return false*)
+let sumcoo_3 = typecheck_cmd ctx (MatSumCOO("d1",Num 3, Var "d2")) (* Should return false*)
+let sumcoo_4 = typecheck_cmd ctx (MatSumCOO("d1",Var "d1", Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return false*)
+
+(*Test MatSubCOO*)
+
+let subcoo_0 = typecheck_cmd ctx (MatSubCOO("d1",Var "d1", Var "d2")) (* Should return true*)
+let subcoo_1 = typecheck_cmd ctx (MatSubCOO("d3",Var "d1", Var "d2")) (* Should return true*)
+let subcoo_2 = typecheck_cmd ctx (MatSubCOO("v",Var "d1", Var "d2")) (* Should return false*)
+let subcoo_3 = typecheck_cmd ctx (MatSumCOO("d1",Num 3, Var "d2")) (* Should return false*)
+let subcoo_4 = typecheck_cmd ctx (MatSubCOO("d1",Var "d1", Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return false*)
+
+(*Test MatMulCOO*)
+
+let mulcoo_0 = typecheck_cmd ctx (MatMulCOO("d1",Var "d1", Var "d2")) (* Should return true*)
+let mulcoo_1 = typecheck_cmd ctx (MatMulCOO("d3",Var "d1", Var "d2")) (* Should return true*)
+let mulcoo_2 = typecheck_cmd ctx (MatMulCOO("v",Var "d1", Var "d2")) (* Should return false*)
+let mulcoo_3 = typecheck_cmd ctx (MatMulCOO("d1",Num 3, Var "d2")) (* Should return false*)
+let mulcoo_4 = typecheck_cmd ctx (MatMulCOO("d1",Var "d1", Vector [1; 7; 0; 0; 0; 2; 8; 0; 5; 0; 3; 9; 0; 6; 0; 4] )) (* Should return false*)
